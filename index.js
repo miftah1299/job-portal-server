86a64832a30987b319ef082d9ff49b56f3a7499f
Your Name <jannat27120@gmail.com>
2024-12-14 22:23:04 +0600

    add job collection and endpoint to retrieve jobs from MongoDB

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fh7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fh7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,13 +29,17 @@ async function run() {
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
 
+        // job collection
+        const jobsCollection = client.db("jobPortal").collection("jobs");
 
-
-
-        
+        app.get("/jobs", async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
