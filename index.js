e9e3c28628d4a17c6631e567c52efb223a8ee12f
Your Name <jannat27120@gmail.com>
2024-12-22 05:13:12 +0600

    update CORS configuration, enhance JWT token handling, and add Vercel deployment configuration

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://job-portal-cd467.web.app",
+            "https://job-portal-cd467.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,9 +27,9 @@ const logger = (req, res, next) => {
 };
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req?.cookies?.token;
     if (!token) {
-        return res.status(401).send({ message: "Access Denied" });
+        return res.status(401).send({ message: "Access Denied, Unauthorized" });
     }
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
@@ -30,6 +39,17 @@ const verifyToken = (req, res, next) => {
     });
     next();
 };
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+// localhost:5000 and localhost:5173 are treated as same site.
+// so sameSite value must be strict in development server.
+// in production sameSite will be none
+// in development server secure will false .
+// in production secure will be true
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fh7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -45,9 +65,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
@@ -60,15 +80,21 @@ async function run() {
             .collection("job-applications");
 
         // Auth apis
+        // Creating token
         app.post("/jwt", async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
                 expiresIn: "5h",
             });
-            res.cookie("token", token, {
-                httpOnly: true,
-                secure: false, // set to true if your using https
-            }).send({ success: true });
+            res.cookie("token", token, cookieOptions).send({ success: true });
+        });
+        // clearing Token
+        app.post("/logout", async (req, res) => {
+            const user = req.body;
+            console.log("logging out", user);
+            res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({
+                success: true,
+            });
         });
 
         // job apis
@@ -79,11 +105,8 @@ async function run() {
 
             if (email) {
                 query = { hr_email: email };
-                const result = await jobsCollection.find(query).toArray();
-                res.send(result);
-                return;
             }
-            const cursor = jobsCollection.find();
+            const cursor = jobsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
@@ -113,10 +136,13 @@ async function run() {
 
         // job application apis
         // get job applications by email
-        app.get("/job-applications", async (req, res) => {
+        app.get("/job-applications", verifyToken, async (req, res) => {
             const email = req.query.email;
             const query = { application_email: email };
             // console.log("cookies: ", req.cookies);
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: "Forbidden Acceess" });
+            }
 
             const result = await jobApplicationsCollection
                 .find(query)
